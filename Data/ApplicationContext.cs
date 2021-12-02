72d197437f7ddcc0e56fb1cbcf82bc22447c97cd
Luiz Felipe de Oliveira <luiz.pepe@hotmail.com>
2021-12-02 17:58:54 -0300

    Criando consultas projetadas

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -1,9 +1,29 @@
+using System;
+using Consultas.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Consultas.Data
 {
     public class ApplicationContext : DbContext
     {
-        
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        //Configura a string de conexão
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data Source=GUIDE-LUIZJEDI;Initial Catalog=Jedi_Consultas;Integrated Security=true;pooling=true";
+            optionsBuilder
+                 .UseSqlServer(strConnection)
+                 .EnableSensitiveDataLogging()
+                 .LogTo(Console.WriteLine, LogLevel.Error);
+        }
+
+        // Filtra as consultas de forma global, de acordo a lógica solicitada
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.Entity<Departamento>().HasQueryFilter(x => !x.Excluido);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace DominandoEFCore
 {
@@ -6,7 +8,71 @@ namespace DominandoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // EnsureCreatedAndDeleted();
+            // FiltroGlobal();
+            // IgnoreFiltroGlobal();
+            ConsultaProjetada();
+        }
+
+        // Criar ou deletar o banco de dados
+        static void EnsureCreatedAndDeleted()
+        {
+            using var db = new Consultas.Data.ApplicationContext();
+
+            db.Database.EnsureCreated();
+            // db.Database.EnsureDeleted();
+        }
+
+        // Criando uma consulta projetada
+        static void ConsultaProjetada()
+        {
+            using var db = new Consultas.Data.ApplicationContext();
+            Consultas.Scripts.Initial.Setup(db);
+
+            var departamentos = db.Departamentos
+                    .Where(x => x.Id > 0)
+                    .Select(x => new { x.Descricao, Funcionarios = x.Funcionarios.Select(f => f.Nome) }) // Seleciona apenas os campos desejados do objeto
+                    .ToList();
+
+            foreach (var departamento in departamentos)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"Descrição: {departamento.Descricao}");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("Funcionários: \r\n");
+
+                foreach (var funcionario in departamento.Funcionarios)
+                {
+                    Console.WriteLine($"\t Nome: {funcionario}");
+                }
+                Console.WriteLine("____________________________");
+            }
+        }
+        // Ignorando filtro Global
+        static void IgnoreFiltroGlobal()
+        {
+            using var db = new Consultas.Data.ApplicationContext();
+            Consultas.Scripts.Initial.Setup(db);
+
+            var departamentos = db.Departamentos.IgnoreQueryFilters().Where(x => x.Id > 0).ToList();
+
+            foreach (var departamento in departamentos)
+            {
+                Console.WriteLine($"Descrição: {departamento.Descricao} \t Excluído: {departamento.Excluido}");
+            }
+        }
+        // Filtro Global
+        static void FiltroGlobal()
+        {
+            using var db = new Consultas.Data.ApplicationContext();
+            Consultas.Scripts.Initial.Setup(db);
+
+            var departamentos = db.Departamentos.Where(x => x.Id > 0).ToList();
+
+            foreach (var departamento in departamentos)
+            {
+                Console.WriteLine($"Descrição: {departamento.Descricao} \t Excluído: {departamento.Excluido}");
+            }
         }
     }
 }
