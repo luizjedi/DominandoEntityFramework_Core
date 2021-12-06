ff2607ae00a6a88656d39cff0192b0bcc0f7158e
Luiz Felipe de Oliveira <luiz.pepe@hotmail.com>
2021-12-06 14:50:16 -0300

    Início trilha Stored Procedures

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Stored_Procedures.Domain;
+
+namespace Stored_Procedures.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        //Configura a string de conexão
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data Source=GUIDE-LUIZJEDI;Initial Catalog=Jedi_StoredProcedures;Integrated Security=true;pooling=true";
+            optionsBuilder
+                 .UseSqlServer(strConnection, p => p.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)) // QuerySplittingBehavior faz a divisão das consultas de forma global
+                 .EnableSensitiveDataLogging()
+                 .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        // Filtra as consultas de forma global, de acordo a lógica solicitada
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.Entity<Departamento>().HasQueryFilter(x => !x.Excluido);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StoredProcedures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnsureCreatedAndDeleted();
+        }
+
+        // Criar ou deletar o banco de dados
+        static void EnsureCreatedAndDeleted()
+        {
+            using var db = new Stored_Procedures.Data.ApplicationContext();
+
+            db.Database.EnsureCreated();
+            // db.Database.EnsureDeleted();
+        }
+    }
+}
