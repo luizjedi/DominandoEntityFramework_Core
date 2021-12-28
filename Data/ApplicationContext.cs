5f4055539a8a97cdc9a0582d410e7356c497fb4b
Luiz Felipe de Oliveira <luiz.pepe@hotmail.com>
2021-12-28 16:39:22 -0300

    Início do módulo Performance

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using performance.Domain;
+using System;
+
+namespace performance.Data
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data Source=GUIDE-LUIZJEDI;Initial Catalog=Jedi_Performance;Integrated Security=true;pooling=true";
+
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using performance.Data;
+using performance.Domain;
+using System;
+using System.Linq;
+
+namespace performance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup();
+            //ConsultaRastreada();
+            //ConsultaNaoRastreada();
+            ConsultaComResolucaoDeIdentidade();
+        }
+
+        // Consulta NÃO Rastreada com resolução de Identidade
+        static void ConsultaComResolucaoDeIdentidade()
+        {
+            using var db = new ApplicationContext();
+
+            var funcionarios = db.Funcionarios.AsNoTrackingWithIdentityResolution().Include(p => p.Departamento).ToList();
+        }
+
+        // Consulta NÃO Rastreada
+        static void ConsultaNaoRastreada()
+        {
+            using var db = new ApplicationContext();
+
+            var funcionarios = db.Funcionarios.AsNoTracking().Include(p => p.Departamento).ToList();
+        }
+
+        // Consulta Rastreada
+        static void ConsultaRastreada()
+        {
+            using var db = new ApplicationContext();
+
+            var funcionarios = db.Funcionarios.Include(p => p.Departamento).ToList();
+        }
+
+        // Setup de criação do banco
+        static void Setup()
+        {
+            using (var db = new ApplicationContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                db.Departamentos.Add(new Departamento
+                {
+                    Descricao = "Departamento Jurídico",
+                    Ativo = true,
+                    Funcionarios = Enumerable.Range(1, 100).Select(p => new Funcionario
+                    {
+                        CPF = p.ToString().PadLeft(11, '0'),
+                        Nome = $"Funcionário {p}",
+                        RG = p.ToString()
+                    }).ToList()
+                });
+                db.SaveChanges();
+            }
+        }
+    }
+}
